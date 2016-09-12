9dc9a72af5e325923c9b21d244f3f73274cab5c2
Andreea Anastasescu <Andreea.Anastasescu@isdcnet.local>
2016-09-12 16:14:18 +0300

    on of :AdventureTravel's Api s Please enter the commit message for your changes. Lines starting

---
+++ Clients/1.client_credentials/Api/Controllers/TopExpeditionsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class TopExpeditionsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            IList<Expedition> expeditions = new List<Expedition>();
+            expeditions.Add(new Expedition() { Name = "Elbrus 2017", DurationInDays = "7-10"});
+            expeditions.Add(new Expedition() { Name = "Mont Blanc 2017", DurationInDays = "5-7"});
+            expeditions.Add(new Expedition() { Name = "Grossglockner 2017", DurationInDays = "5-7" });
+            expeditions.Add(new Expedition() { Name = "Negoiu 2016", DurationInDays = "2-3"});
+            expeditions.Add(new Expedition() { Name = "Piatra Craiului Tour", DurationInDays = "3-4" });
+            expeditions.Add(new Expedition() { Name = "Retezat Winter School", DurationInDays = "5" });
+            return Ok<IList<Expedition>>(expeditions);
+        }
+
+        public class Expedition
+        {
+            public string Name { set; get; }
+            public string DurationInDays { set; get; }
+        }
+    }
+}
---
+++ Clients/1.client_credentials/Api/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using Api;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Api.Startup))]
+namespace Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
