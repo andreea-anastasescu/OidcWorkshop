1d89ed5853441b797d10ac230d75fc3617c23b30
Andreea Anastasescu <Andreea.Anastasescu@isdcnet.local>
2016-09-13 23:20:14 +0300

    first version, yet incomplete`

---
+++ Clients/2.implicit_flow/MvcClient/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using MvcClient;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+
+            string idPUrl = "https://localhost:44333/core";
+            string clientUrl = "https://9122";
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationType = "Cookies",
+                CookieName = "mvc.implicit.cookie"
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions()
+            {
+                ClientId = "mvc.owin.implicit",
+                Authority = idPUrl,
+                SignInAsAuthenticationType = "Cookies",
+                RedirectUri = clientUrl,
+                ResponseType = "id_token",
+                Scope = "openid email",
+                UseTokenLifetime = false
+            });
+        }
+    }
+}
